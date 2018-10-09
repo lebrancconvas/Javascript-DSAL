2dfb4f1a8a14bdee6850275d16ac34d22e9d6a79
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-10-10 06:00:08 +0700

    Complete Create Queue.

--- queue.js
+++ queue.js
@@ -1,10 +1,49 @@
+// Create Queue
 function Queue() {
     let queue = []
     this.enqueue = function(e) {
         queue.push(e)
     }
 
-    this.dequeue = function(e) {
+    this.dequeue = function() {
         queue.shift()
     }
+
+    this.front = function() {
+        return queue[0]
+    }
+
+    this.rear = function() {
+        return queue[queue.length - 1]
+    }
+
+    this.print = function() {
+        console.log(queue)
+    }
+
+    this.isEmpty = function() {
+        return queue.length === 0
+    }
 }
+
+// Queue Operation
+let food = new Queue()
+console.log(food.isEmpty())
+
+// Add Element to The Queue
+food.enqueue("Soba")
+food.enqueue("Sushi")
+food.enqueue("Curry")
+food.enqueue("Chocolate")
+food.enqueue("Soup")
+console.log(food.front())
+console.log(food.rear())
+food.print()
+
+// Remove Element to The Queue
+food.dequeue()
+food.dequeue()
+console.log(food.front())
+console.log(food.rear())
+food.print()
+console.log(food.isEmpty())
